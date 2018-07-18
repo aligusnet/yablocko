8a120947d418f4ea159a6ccd2b5654a22c6a33ae
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-07-18 23:28:42 +0100

    some comments for Block module

--- Yablocko/Block.fs
+++ Yablocko/Block.fs
@@ -6,11 +6,17 @@ open System.Text
 
 module Block =
     type T = {
+        /// Index of the block in the chain
         Index : int
+        /// Creation date and time of the block
         TimeStamp : DateTime
+        /// Hash value of the previous block from the chain. Empty for the initial block.
         PreviousHash : string
+        /// Pre-computed hash value used to validate the block
         Hash : string
+        /// Block's payload
         Data : string
+        /// Value that brings the required number of leading zeroes to the hash value
         Nonce : int
     }
 
@@ -21,6 +27,8 @@ module Block =
             |> sha256.ComputeHash
             |> Convert.ToBase64String
 
+    /// Mine function returns a hash value with number of leading zeroes equals value of difficulty
+    /// and value of nonce that required to get such number of leading zeroes in the hash
     let private mine (timeStamp : DateTime) previousHash data difficulty =
         let leading = String.replicate difficulty "0"
         let calc = calculateHash timeStamp previousHash data
@@ -32,6 +40,17 @@ module Block =
 
         helper 0
 
+    
+    /// **Description**
+    /// Create new block
+    /// **Parameters**
+    ///   * `difficulty` - a number of leading zeroes in hash value.
+    ///   * `timeStamp` - timestamp of the block creation
+    ///   * `data` - payload
+    ///   * `previous` - previous block in the chain
+    ///
+    /// **Output Type**
+    ///   * `T`
     let create difficulty (timeStamp : DateTime) data (previous : T) = 
         let hash, nonce = mine timeStamp previous.Hash data difficulty
         {
@@ -43,6 +62,19 @@ module Block =
             Nonce = nonce
         }
 
+    
+    /// **Description**
+    /// Create initial block in the chain.
+    /// **Parameters**
+    ///   * `difficulty` - a number of leading zeroes in hash value.
+    ///   * `timeStamp` - timestamp of the block creation
+    ///   * `data` - payload
+    /// 
+    /// **Output Type**
+    ///   * `T`
+    ///
+    /// **Exceptions**
+    /// No exceptions
     let createInitial difficulty (timeStamp : DateTime) data =
         let previousHash = ""
         let hash, nonce = mine timeStamp previousHash data difficulty
@@ -55,5 +87,17 @@ module Block =
             Nonce = nonce
         }
 
+    
+    /// **Description**
+    /// Tests if the current block valid;.
+    /// **Parameters**
+    ///   * `previousHash` - hash of the previous block in the chain
+    ///   * `block` - block to be tested
+    ///
+    /// **Output Type**
+    ///   * `bool` - true if block is valid
+    ///
+    /// **Exceptions**
+    /// No exceptions
     let isValid previousHash (block : T) =
         block.PreviousHash = previousHash && block.Hash = calculateHash block.TimeStamp block.PreviousHash block.Data block.Nonce
